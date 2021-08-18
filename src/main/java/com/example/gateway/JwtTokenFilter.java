f7df040735739b5c23563733785378a8e9456361
anas1606 <darshan@eliteevince.in>
2021-08-18 20:06:31 +0530

    Role Base JWT FILTER

--- src/main/java/com/example/gateway/JwtTokenFilter.java
+++ src/main/java/com/example/gateway/JwtTokenFilter.java
@@ -3,7 +3,6 @@ package com.example.gateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
