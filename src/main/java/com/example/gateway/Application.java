4d91228ececc5e64f14ce48a34e1575095fc231a
anas1606 <darshan@eliteevince.in>
2021-08-20 16:39:41 +0530

    Swagger

--- src/main/java/com/example/gateway/Application.java
+++ src/main/java/com/example/gateway/Application.java
@@ -3,13 +3,14 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
